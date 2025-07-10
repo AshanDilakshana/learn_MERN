6d8a0f964695c1d20d78d0768e5f5e783d7487ce
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-07-10 13:03:46 +0530

    cc

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -3,11 +3,39 @@ import express from "express";
 const app = express();
 
 
+app.use(express.json()) //middle ware *eawana data tika piliwelata hadana ek karanne
+
+app.get("/",
+    (req, res)=>{
+
+        console.log("request body",req.body);
+        console.log("get request  recived");
+
+        res.json(   //send the message sender
+            {
+                 message : "get reques fetch sucessful"
+            }
+        )
+     });
 
 
+// POST Route
+app.post("/", (req, res) => {
+  console.log("POST request received");
+
+  console.log("Data:", req.body); // Logs data sent from Postman
+
+  res.json({ message: "POST request received", data: req.body });
+});
 
 
+// DELETE Route
+app.delete("/", (req, res) => {
 
+  console.log("DELETE request received");
+
+  res.json({ message: "DELETE request acknowledged" });
+});
 
 
 
@@ -25,3 +53,5 @@ const app = express();
 app.listen(5000, () => {
   console.log("server is started !");
 });
+
+setInterval(() => {}, 1000 * 60 * 60);
