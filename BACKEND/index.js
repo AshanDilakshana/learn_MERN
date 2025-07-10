bce2edd20bb7e9b27a8902a6845fa32d56c2396f
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-07-10 13:05:15 +0530

    add mongoose

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,14 +1,10 @@
-import express from "express";
-
-const app = express();
-
+import express from "express"
+import mongoose from "mongoose";
 
 
+const app =express()
 
-
-
-
-
+app.use(express.json())
 
 
 
