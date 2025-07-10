bb23c6ad7ab415ed3d5079a6cc9f5353f74fe86a
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-07-10 14:57:25 +0530

    4 th day commits

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,14 +1,29 @@
-import mongoose from "mongoose";
-
 import express from "express";
+import mongoose from "mongoose";
+import Student from "./model/student.js";
 
 const app = express();
 
-
 app.use(express.json()) //middle ware *eawana data tika piliwelata hadana ek karanne
 
+const connectionString = "mongodb+srv://admin:123@study.i1em97j.mongodb.net/?retryWrites=true&w=majority&appName=study"
+
+mongoose.connect(connectionString) //create data base connection 
+.then(
+  ()=>{
+     console.log("database connected !");
+  }
+ 
+).catch(
+  ()=>{
+    console.log("database connction err");
+  }
+)
+
+
+
 app.get("/",
-    (req, res)=>{
+      (req, res)=>{
 
         console.log("request body",req.body);
         console.log("get request  recived");
@@ -22,26 +37,45 @@ app.get("/",
 
 
 // POST Route
+
 app.post("/", (req, res) => {
-  console.log("POST request received");
+
+  const student = new Student(
+    {
+      name : req.body.name,
+      age : req.body.age,
+      city : req.body.city
+    }
+  ) 
+    student.save(). then(
+      ()=>{
+        res.json(
+          {message : "studet create sucsess fully"}
+        )
+      }
+    ).catch(
+      ()=>{
+        res.json(
+          {message : "studet create error"}
+        )
+      }
+    )
 
   console.log("Data:", req.body); // Logs data sent from Postman
 
-  res.json({ message: "POST request received", data: req.body });
+  //res.json({ message: "POST request received", data: req.body });
 });
-
 
 
 // DELETE Route
 app.delete("/", (req, res) => {
-
 
   console.log("DELETE request received");
 
   res.json({ message: "DELETE request acknowledged" });
 });
 
-app.use(express.json())
+
 
 
 
