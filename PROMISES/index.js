b6158296e815744ea6ad34a0c00eb43b004f7a55
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-07-28 00:41:15 +0530

    day _7 promise in js

---
+++ PROMISES/index.js
@@ -0,0 +1,75 @@
+
+/*
+setTimeout(
+    () => {
+        console.log('helloworld');
+    },5000
+    
+) */
+
+    /*
+  const employees = {
+    name: 'John',
+    age: 30,
+    car:{
+        make: 'Toyota',
+        model: 'Camry',
+        year: 2020,
+        price: 20000
+    }
+  }  
+console.log("befor include tax "+employees.car.price);
+console.log("after include tax ");
+  console.log(employees.car.price +1000);
+  */
+ 
+  /*
+let winner = "ashan ";
+let isjudgeboardOK= true;
+
+  const mypromises =  new promise(
+    (resolve , reject)=>{
+
+      if(isjudgeboardOK){
+        settimeout(
+            () => {
+                console.log("the winner is " + winner);
+            }, 5000
+
+            resolve()
+        )
+      }else{
+        reject()
+        }
+        
+    }
+
+  )
+
+  mypromises.then(
+    () => {
+        console.log("congratulations " );
+    }).catch(
+        (error) => {
+            console.log("sorry " + error);
+        })*/
+
+
+            // function grtuser(){
+            //   const  mypromise = new Promise()
+            //   (resolve , reject) => {
+
+            //   return mypromise();
+            // }
+
+            // grtuser().then(
+            //     ()=>{
+
+            //     }
+            //   ).catch(
+            //     ()=> {
+                    
+            //     }
+
+            //   )
+            // }
