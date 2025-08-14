8556eff300d5652084fd2d6d5f834c2c5bace8fe
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-08-14 19:03:48 +0530

    Day 12 - fronend

--- fronend/src/App.jsx
+++ fronend/src/App.jsx
@@ -7,9 +7,24 @@ function App() {
   
   return ( 
     <>
-    <h1>wellcome ashan new project</h1>
+    <h1 className='text-red-500 text-5xl'>wellcome ashan new project</h1>
     <ProductCard name = "apple i phone" price = "$200" image = "https://i0.wp.com/gearz.lk/wp-content/uploads/2024/09/iPhone-16-_-iPhone-16-Plus_Pink_1.webp?fit=700%2C800&ssl=1" />
     <ProductCard />
+        <div className='h-[700px] w-[700px] border-[5px] bg-amber-200 relative flex flex-row justify-center items-center'>
+
+
+          <div className='h-[200px] w-[400px] bg-blue-500 relative'>
+            <h1 className='text-white text-4xl f '> Your time is over</h1>
+            <button className='bg-red-500 absolute top-[0px] right-[0px]'>X</button>
+
+          </div>
+
+
+          
+          
+          
+        </div>
+
     </>
    )
 }
--- fronend/src/components/productCard.jsx
+++ fronend/src/components/productCard.jsx
@@ -2,9 +2,10 @@ export default function ProductCard(props) { //funtion name must be capitalized
 
     return(
         <div>
-            <h1>Product Card</h1>
-             <h4>price = {props.price}</h4>
-            <h4>name = {props.name}</h4>
+            <h1 className="text-4xl ">Product Card</h1>
+             <h4 className="text-2xl ">price = {props.price}</h4>
+            <h4 className="text-2xl ">name = {props.name}</h4>
+            
             <img src={props.image} alt="Product Image" />
             <hr></hr>
         </div>
